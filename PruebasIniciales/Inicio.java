e177e4a2077951c5f6dcbe157b3e5f15a478f6a0
Fernandezvalerio <139719967+Fernandezvalerio@users.noreply.github.com>
2024-07-14 20:48:57 -0500

    pagina_inicio_prueba

--- PruebasIniciales/Inicio.java
+++ PruebasIniciales/Inicio.java
@@ -1,4 +1,4 @@
-package MIO;
+package PruebasIniciales;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,7 +16,7 @@ public class Inicio extends JPanel {
     public Inicio() {
         try {
             // Cargar la imagen desde los recursos
-            imagenFondo = ImageIO.read(getClass().getResource("/imagenes/image.jpg"));
+            imagenFondo = ImageIO.read(getClass().getResource("/imagenes/img1_fondo.jpg"));
             if (imagenFondo == null) {
                 System.out.println("No se pudo cargar la imagen.");
             }
@@ -29,24 +29,31 @@ public class Inicio extends JPanel {
         
         // Crear y configurar los botones
         JButton iniciarJuegoButton = new JButton("Iniciar Juego");
-        iniciarJuegoButton.setBounds(450, 250, 200, 50);
-        iniciarJuegoButton.setFont(new Font("Arial", Font.BOLD, 20));
+        iniciarJuegoButton.setBounds(610, 400, 300, 70);
+        iniciarJuegoButton.setFont(new Font("Arial", Font.BOLD, 40));
         iniciarJuegoButton.setBackground(Color.RED);
         iniciarJuegoButton.setForeground(Color.WHITE);
 
+        JButton salirButton = new JButton("Salir");
+        salirButton.setBounds(610, 550, 300, 70);
+        salirButton.setFont(new Font("Arial", Font.BOLD, 40));
+        salirButton.setBackground(Color.RED);
+        salirButton.setForeground(Color.WHITE);
+
         JButton logrosButton = new JButton("Logros");
-        logrosButton.setBounds(450, 320, 200, 50);
+        logrosButton.setBounds(80, 690, 200, 50);
         logrosButton.setFont(new Font("Arial", Font.BOLD, 20));
         logrosButton.setBackground(Color.RED);
         logrosButton.setForeground(Color.WHITE);
 
-        JButton salirButton = new JButton("Salir");
-        salirButton.setBounds(450, 390, 200, 50);
-        salirButton.setFont(new Font("Arial", Font.BOLD, 20));
-        salirButton.setBackground(Color.RED);
-        salirButton.setForeground(Color.WHITE);
+        JButton CreditosButton = new JButton("Creditos");
+        CreditosButton.setBounds(1250, 690, 200, 50);
+        CreditosButton.setFont(new Font("Arial", Font.BOLD, 20));
+        CreditosButton.setBackground(Color.RED);
+        CreditosButton.setForeground(Color.WHITE);
 
         // Agregar los botones al panel
+        add(CreditosButton);
         add(iniciarJuegoButton);
         add(logrosButton);
         add(salirButton);
@@ -66,17 +73,18 @@ public class Inicio extends JPanel {
         }
         // Dibujar el título y subtítulo
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 40));
-        g.drawString("PluzTriviaPeru", 350, 100);
-        g.setFont(new Font("Arial", Font.BOLD, 20));
-        g.drawString("Aprende el que pregunta Edición Perú", 330, 140);
+        g.setFont(new Font("Arial", Font.BOLD, 100));
+        g.drawString("PluzTriviaPeru", 425, 200);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString("Aprende el que pregunta Edición Perú", 500, 300);
     }
 
     // Método para crear y mostrar la interfaz gráfica de usuario (GUI)
+    // Panel base para crear todo
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("PluzTriviaPeru");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1550, 820);
         frame.add(new Inicio());
         frame.setVisible(true);
     }
