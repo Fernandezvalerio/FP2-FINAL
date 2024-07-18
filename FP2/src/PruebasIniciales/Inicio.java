bbc76f2b9134cbeba7d74c5ea75d4c3092217e3c
Andres Raul Ore Soto <139734325+Xandresx1@users.noreply.github.com>
2024-07-18 01:10:33 -0500

    Update Inicio.java

--- FP2/src/PruebasIniciales/Inicio.java
+++ FP2/src/PruebasIniciales/Inicio.java
@@ -320,53 +320,66 @@ public class Inicio extends JPanel {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                ImageIcon backgroundIcon = new ImageIcon(getClass().getResource(LOGROS_IMAGE_PATH));
-                Image background = backgroundIcon.getImage();
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+                ImageIcon backgroundIcon = null;
+                try {
+                    backgroundIcon = new ImageIcon(getClass().getResource(LOGROS_IMAGE_PATH));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (backgroundIcon != null) {
+                    Image background = backgroundIcon.getImage();
+                    g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+                }
             }
         };
         overlayPanel.setOpaque(false);
         overlayPanel.setBounds(0, 0, getWidth(), getHeight());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
-
+    
         // Panel con la tabla centrada
         JPanel logrosPanel = new JPanel(new BorderLayout());
         logrosPanel.setOpaque(false);
-
+    
         String[] columnNames = {"Nombre", "Puntaje"};
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-
+    
         try (Connection conn = Conexion.getConnection()) {
             String query = "SELECT nombre, puntaje FROM usuarios ORDER BY puntaje DESC";
             PreparedStatement stmt = conn.prepareStatement(query);
             ResultSet rs = stmt.executeQuery();
-
+    
             while (rs.next()) {
                 String nombre = rs.getString("nombre");
                 int puntaje = rs.getInt("puntaje");
                 model.addRow(new Object[]{nombre, puntaje});
             }
         } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Error al cargar los logros", "Error", JOptionPane.ERROR_MESSAGE);
             ex.printStackTrace();
         }
-
+    
         JTable table = new JTable(model);
+        table.setFillsViewportHeight(true);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 300));
         JScrollPane scrollPane = new JScrollPane(table);
         logrosPanel.add(scrollPane, BorderLayout.CENTER);
-
+    
         // Agregar botón Salir
         JButton salirButton = new JButton("Salir");
-        salirButton.addActionListener(e -> mostrarPanelInicio());
+        salirButton.addActionListener(e -> {
+            layeredPane.remove(overlayPanel);
+            mostrarPanelInicio();
+        });
         logrosPanel.add(salirButton, BorderLayout.SOUTH);
-
+    
         gbc.gridx = 0;
         gbc.gridy = 0;
         overlayPanel.add(logrosPanel, gbc);
-
+    
         layeredPane.removeAll();
-        layeredPane.add(fxPanel, Integer.valueOf(1)); // Asegurarse de que el video esté detrás
-        layeredPane.add(overlayPanel, Integer.valueOf(2)); // Añadir el panel de botones en la capa superior
+        layeredPane.add(fxPanel, Integer.valueOf(1));
+        layeredPane.add(overlayPanel, Integer.valueOf(2));
         layeredPane.repaint();
         layeredPane.revalidate();
     }
